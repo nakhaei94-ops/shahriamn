0f4915b77733c63f58fd68af19007a21bfac742c
Nakha <nakhaei94@gmail.com>
2026-02-14 19:22:15 +0330

    update welcome system

--- bot.py
+++ bot.py
@@ -1,26 +1,25 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ChatMemberHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 TOKEN = "8065966447:AAEfmJWG_JIGN038gZtftpzVTmg5bGF-wW8"
 
+# پیام خوش‌آمدگویی برای کاربر جدید
 async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    result = update.chat_member
+    if update.message.new_chat_members:
+        member_count = await context.bot.get_chat_member_count(update.effective_chat.id)
+        for member in update.message.new_chat_members:
+            text = (
+                f"{member.first_name} عزیز 🌿\n\n"
+                f"به شهری امن خوش آمدی!\n"
+                f"حالا ما {member_count} نفر هستیم که تصمیم گرفتیم آگاهی‌مان را بالا ببریم و یک شهر امن بسازیم!"
+            )
+            await update.message.reply_text(text)
 
-    # فقط وقتی عضو جدید شد
-    if result.new_chat_member.status == "member":
-        user = result.new_chat_member.user
-        chat = update.effective_chat
-
-        name = user.first_name or "شهروند"
-
-        text = f"{name} به شهری امن خوش اومدی!\n" \
-               f"فعلاً ربات پیام خوش‌آمد را تست می‌کند!"
-
-        # پیام تست
-        await context.bot.send_message(chat.id, text)
-
+# ساخت اپلیکیشن
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(ChatMemberHandler(welcome, ChatMemberHandler.CHAT_MEMBER))
+# اضافه کردن هندلر خوش‌آمدگویی
+app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
 
+# اجرای ربات
 app.run_polling()
